184ff0af9e26847d3a4e80d92b557a44286384e0
Dagnu1 <rasdagnu@gmail.com>
2022-06-02 12:23:54 -0700

    C-Hello-World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - print the given sentence
+  *
+  *Return: always 0
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
